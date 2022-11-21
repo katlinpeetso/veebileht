ec2da90ff91aaeca90efc532a41b5f488b7e5719
Johanna Kaare <johanna.kaare@gmail.com>
2022-11-21 09:55:04 +0200

    uus

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
+/* Kood on pärit lehelt stackowerflow.com (https://stackoverflow.com/questions/49661468/search-a-paragraph),
+koodi autori kasutajanimi on dave, kood on üles laetud 04.04.2018. Kood otsib lehelt otsingukasti 
+sisestatud sõna, ütleb, kui see sõna on lehel olemas ja mitu korda seda lehel esineb ning tõstab vastavad
+sõnad esile. */
+
 function Search() 
 {
     var elements = document.querySelectorAll(".main");
     let search = document.getElementById('searchtext').value;
-    for (var i = 0; i < elements.length; i++)
-    {
-        if(elements[i].innerHTML.toLowerCase().indexOf(search.toLowerCase()) > - 1)
-    {
-        alert("found");
-    }
-    }
-
 }
 
 document.getElementById('searchbutton').addEventListener('click', Search);
@@ -30,7 +27,6 @@ function highlight()
             var main = elements[i].innerHTML;
             var new_text = main.replace(new RegExp('(' + text + ')', 'gi'), '<span class="highlight">$1</span>');
             elements[i].innerHTML = new_text;
-            alert(elements[i].querySelectorAll('.highlight').length + ' occurences found');
         }
         
     }
